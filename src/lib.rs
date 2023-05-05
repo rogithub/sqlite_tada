aee25751005ce867049917d5047c299f7ac75d2b
rockdrigo <correo.rodrigo@gmail.com>
2023-05-05 17:45:01 -0500

    playing code

--- src/lib.rs
+++ src/lib.rs
@@ -1,11 +1,10 @@
-use rusqlite::{Connection, Result, Error};
+use rusqlite::{Connection, Result, Error, Params};
 
-pub fn create_table(conn: Connection) -> Result<usize, Error> {
-    let query = "
-        CREATE TABLE users (name TEXT, age INTEGER);
-        INSERT INTO users ('Alice', 42);
-        INSERT INTO users ('Bob', 69);
-    ";
+pub fn create_table(conn: &Connection, query: &str) -> Result<usize, Error> {    
+    conn.execute(&query, ())
+}
+
+pub fn insert_record(conn: &Connection, query: &str) -> Result<usize, Error> {    
     conn.execute(&query, ())
 }
 
@@ -15,8 +14,13 @@ mod tests {
 
     #[test]
     fn create_table_test() {
+        let q_create = "CREATE TABLE users (name TEXT, age INTEGER);";
+        //let q_insert = "INSERT INTO users ('Alice', 42);";
         let conn = Connection::open_in_memory().unwrap();
-        let result = create_table(conn);
+        let result = create_table(&conn, &q_create);
         assert_eq!(result, Ok(0));
+
+        //let result1 = insert_record(&conn, &q_insert);
+        //assert_eq!(result1, Ok(0));
     }
 }
