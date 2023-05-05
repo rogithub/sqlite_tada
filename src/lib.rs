d51630f651988a488c239084728769440f8e9ce8
rockdrigo <correo.rodrigo@gmail.com>
2023-05-05 17:08:00 -0500

    playing code

--- src/lib.rs
+++ src/lib.rs
@@ -1,5 +1,12 @@
-pub fn add(left: usize, right: usize) -> usize {
-    left + right
+use rusqlite::{Connection, Result, Error};
+
+pub fn create_table(conn: Connection) -> Result<usize, Error> {
+    let query = "
+        CREATE TABLE users (name TEXT, age INTEGER);
+        INSERT INTO users ('Alice', 42);
+        INSERT INTO users ('Bob', 69);
+    ";
+    conn.execute(&query, ())
 }
 
 #[cfg(test)]
@@ -7,8 +14,9 @@ mod tests {
     use super::*;
 
     #[test]
-    fn it_works() {
-        let result = add(2, 2);
-        assert_eq!(result, 4);
+    fn create_table_test() {
+        let conn = Connection::open_in_memory().unwrap();
+        let result = create_table(conn);
+        assert_eq!(result, Ok(0));
     }
 }
